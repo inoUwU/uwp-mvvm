0188e507f0b581c4c462f905694f24dcd8b56f84
ino <87593133+inoUwU@users.noreply.github.com>
2023-10-16 22:41:20 +0900

    プロジェクト ファイルを追加します。

---
+++ uwp-mvvm/Calculator.cs
@@ -0,0 +1,14 @@
+namespace uwp_mvvm
+{
+    class Calculator
+    {
+        private int value1 = 0;
+        private int value2 = 0;
+
+        public Calculator(int val1, int val2)
+        {
+            this.value1 = val1;
+            this.value2 = val2;
+        }
+    }
+}
