137b9e26c6044ddebc04c2aebc5e192396aa04dd
ino <87593133+inoUwU@users.noreply.github.com>
2023-10-24 23:24:49 +0900

    calculatorクラス作成

--- uwp-mvvm/Calculator.cs
+++ uwp-mvvm/Calculator.cs
@@ -1,6 +1,6 @@
 namespace uwp_mvvm
 {
-    class Calculator
+    public class Calculator
     {
         private int value1 = 0;
         private int value2 = 0;
@@ -9,6 +9,26 @@ namespace uwp_mvvm
         {
             this.value1 = val1;
             this.value2 = val2;
+        }
+
+        public int Add()
+        {
+            return value1 + value2;
+        }
+
+        public int Sub()
+        {
+            return value1 - value2;
+        }
+
+        public int Mul()
+        {
+            return value1 * value2;
+        }
+
+        public int Div()
+        {
+            return value1 / value2;
         }
     }
 }
